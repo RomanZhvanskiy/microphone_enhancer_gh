07f72a452e70cfa1bda6907609763f6df2e32f26
Arthur Briens <a.brienst@gmail.com>
2024-02-07 14:36:57 +0100

    metrics are up

--- image_metrics/img_metrics.py
+++ image_metrics/img_metrics.py
@@ -4,7 +4,7 @@ import numpy as np
 def image_res(im1, im2, meter):
     print(f'calculating similarity between the two images based on {meter}')
     if meter == "SSIM":
-        ssim_score, _ = compare_similarity(im1, im2, full=True)
+        ssim_score, _ = compare_similarity(im1, im2, full=True, data_range=im1.max() - im1.min())
         print("SSIM score : ", ssim_score)
     if meter == "PSNR":
         im1 = im1.astype(np.float64)
@@ -13,5 +13,3 @@ def image_res(im1, im2, meter):
         max_pixel = 255.0
         psnr = 20 * np.log(max_pixel / np.sqrt(mse))
         print(f"PSNR score : ", psnr)
-    else:
-        print('ERROR: please select SSIM or PSNR as metrics')
