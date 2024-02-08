772d0b75dba161ae65b8a0010a79311476960f78
Roman Zhvanskiy <rz03@outlook.com>
2024-02-08 12:37:15 +0000

    aaa

--- params.py
+++
@@ -1,6 +0,0 @@
-import os
-
-audio_data_folder = 'audio_data'
-audio_data_folder_path = os.path.join(os.path.dirname(__file__), audio_data_folder)
-audio_in_path = os.path.join(audio_data_folder_path, 'audio_in')
-audio_out_path = os.path.join(audio_data_folder_path, 'audio_out')
