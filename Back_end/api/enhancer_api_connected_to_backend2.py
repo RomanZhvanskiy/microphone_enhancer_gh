0e38812e38b325ff1b2184ec005ae4ed50c98715
Valentin Golobokin <golobokin@gmail.com>
2024-02-14 14:09:34 +0100

    fe updated, shows pics for hugging

--- Back_end/api/enhancer_api_connected_to_backend2.py
+++ Back_end/api/enhancer_api_connected_to_backend2.py
@@ -13,41 +13,27 @@ from hugging_models.hugrestore import dns4_16k, wham_16k
 from interface.audioenhancer_local import  pred_for_api
 app = FastAPI()
 
-#app.mount("/audio_in", StaticFiles(directory=audio_in_path), name="audio_in")
+app.mount("/audio_in", StaticFiles(directory=audio_in_path), name="audio_in")
 app.mount("/audio_out", StaticFiles(directory=audio_out_path), name="audio_out")
 
 @app.get('/') # root endpoint
 def index():
     return {'It works!': True}
 
-@app.get('/enhance') # enhancer endpoint
-def index():
-    return {'Enhancer endpont': True}
-
-@app.get("/audio_in") # list files in audio_in deirectory
+@app.get("/audio_in_list") # list files in audio_in deirectory
 def audio_in():
     return list_path(audio_in_path)
 
-# @app.get("/audio_out") # list files in audio_out deirectory
-# def audio_in():
-#     return list_path(audio_out_path)
+@app.get("/audio_out_list") # list files in audio_out deirectory
+def audio_in():
+    return list_path(audio_out_path)
 
 @app.post("/upload_file")
 async def upload_file(enhancer: str = Form(...), file: UploadFile = File(...)):
-    #import soundfile as sf
-    #data, sr = sf.read(file.file)
-
     # temp file logic
     temp = NamedTemporaryFile(delete=False)
     with temp as f:
         f.write(file.file.read())
-
-    # normal file logic
-    # file_name = os.path.join(audio_in_path, file.filename)
-    # with open(file_name, "x") as audio_file:
-    #     audio_file.write(file.file.read())
-
-    #import ipdb; ipdb.set_trace()
 
     if enhancer == "speechbrain/sepformer-wham16k-enhancement":
         spec_aud_sr, cleaned_path = wham_16k(temp.name, from_fs=False)
@@ -61,17 +47,6 @@ async def upload_file(enhancer: str = Form(...), file: UploadFile = File(...)):
         #import ipdb; ipdb.set_trace()
         spec_aud_sr, cleaned_path = pred_for_api(where_to_find_bad_audio=temp.name, enhancer="microphone_enhancer_gh/conv_autoencoder_16_32_64_32_16_1")
         #import ipdb; ipdb.set_trace()
-
-    elif enhancer == "NOT IMPLEMENTED YET: microphone_enhancer_gh":
-        False #spec_aud_sr, cleaned_path = dns4_16k(temp.name, from_fs=False)
-
-    # headers = {'Content-Disposition': 'inline'}
-    # return FileResponse(cleaned_path, media_type="audio/wav", headers = headers)
-
-    # with open(cleaned_path, "rb") as f:
-    #     contents = f.read()
-    # headers = {'Content-Disposition': f'attachment; filename="test"'}
-    # return Response(contents, headers=headers, media_type='audio/wav')
 
     return {"cleaned_file_name": os.path.basename(cleaned_path),
             "sample rate": spec_aud_sr[2],
--- Back_end/hugging_models/hugrestore.py
+++ Back_end/hugging_models/hugrestore.py
@@ -5,17 +5,15 @@ This module cleans the audio file using SpeechBrain pre-trained models from Hugg
     dns4_16k: https://huggingface.co/speechbrain/sepformer-dns4-16k-enhancement
 """
 
-#from scipy.io.wavfile import write
-#import numpy as np
 import os
 import sys
 import shutil
-import tempfile
 
 import torchaudio
 import librosa
-#import soundfile as sf
 from speechbrain.pretrained import SepformerSeparation as separator
+import matplotlib.pyplot as plt
+import numpy as np
 
 from audio_preprocessing import preprocessing as pp
 from params import *
@@ -25,12 +23,6 @@ def check_folders():
     """
     Checks if audio directories exist and if not - creates them.
     """
-    # now in params.py
-    # audio_data_folder = 'audio_data'
-    # audio_data_folder_path = os.path.join(os.path.dirname(__file__), '..', audio_data_folder)
-    # audio_in_path = os.path.join(audio_data_folder_path, 'audio_in')
-    # audio_out_path = os.path.join(audio_data_folder_path, 'audio_out')
-
     if not os.path.exists(audio_data_folder_path):
         os.makedirs(audio_data_folder_path)
 
@@ -40,18 +32,6 @@ def check_folders():
     if not os.path.exists(audio_out_path):
         os.makedirs(audio_out_path)
 
-    #return audio_in_path, audio_out_path
-
-# def check_file(input_file:str):
-#     """
-#     Checks if nooisy file exists in 'audio_data/audio_in' and if not - exits.
-#     """
-    # if not os.path.isfile(os.path.join(audio_in_path, input_file + '.wav')):
-    #     return False
-    #     # need to change handling of this exception to something reasonabel (now stops the program)
-    #     sys.exit(f'File {input_file}.wav does not exist, please make sure the file is in the '\
-    #            '"audio_in" folder and if the name (without .wav extension) is spelled correctly')
-    # return os.path.isfile(os.path.join(audio_in_path, input_file + '.wav'))
 
 def wham_16k(input_file:str, from_fs:bool=True):
     """
@@ -67,23 +47,20 @@ def wham_16k(input_file:str, from_fs:bool=True):
         check_folders()
         if os.path.isfile(os.path.join(audio_in_path, input_file + '.wav')):
             full_input_file = os.path.join(audio_in_path, input_file + '.wav')
-            full_output_file = os.path.join(audio_out_path, input_file + '-wham_16k-res' + '.wav')
+            full_output_file = os.path.join(audio_out_path, input_file + '-wham_16k' + '.wav')
         else:
             sys.exit(f'File {input_file}.wav does not exist')
     else:
         full_input_file = input_file
-        full_output_file = os.path.join(audio_out_path, os.path.basename(input_file) + '-wham_16k-res' + '.wav')
+        full_output_file = os.path.join(audio_out_path, os.path.basename(input_file) + '-wham_16k' + '.wav')
 
-
-    shutil.copyfile(os.path.join(full_input_file), os.path.join(audio_in_path, os.path.basename(input_file) + '-wham_16k-orig' + '.wav'))
+    shutil.copyfile(os.path.join(full_input_file), os.path.join(audio_in_path, os.path.basename(input_file) + '-wham_16k' + '.wav'))
+    save_spectro_image(os.path.join(audio_in_path, os.path.basename(input_file) + '-wham_16k' + '.wav'))
     model = separator.from_hparams(source="speechbrain/sepformer-wham16k-enhancement",
                                    savedir='pretrained_models/sepformer-wham16k-enhancement')
     est_sources = model.separate_file(full_input_file) # autoconverts to 16kHz
     torchaudio.save(full_output_file, est_sources[:, :, 0].detach().cpu(), 16000)
-
-    #sf.write(full_output_file, est_sources[:, :, 0][0], 16000, subtype='PCM_16')
-    # print(f"File restored with 'speechbrain/sepformer-dns4-16k-enhancement' and saved as "\
-    #     f"'{input_file}-dns4-16k-res.wav' in '{audio_data_folder}/audio_out' folder.")
+    save_spectro_image(full_output_file)
 
     return wav_to_spectro(full_output_file, 16000), full_output_file
 
@@ -102,26 +79,21 @@ def dns4_16k(input_file:str, from_fs:bool=True):
         check_folders()
         if os.path.isfile(os.path.join(audio_in_path, input_file + '.wav')):
             full_input_file = os.path.join(audio_in_path, input_file + '.wav')
-            full_output_file = os.path.join(audio_out_path, input_file + '-dns4-16k-res' + '.wav')
+            full_output_file = os.path.join(audio_out_path, input_file + '-dns4-16k' + '.wav')
         else:
             sys.exit(f'File {input_file}.wav does not exist')
     else:
         full_input_file = input_file
-        #full_output_file = os.path.join(tempfile.gettempdir(), input_file + '-dns4-16k-res' + '.wav')
-        full_output_file = os.path.join(audio_out_path, os.path.basename(input_file) + '-dns4-16k-res' + '.wav')
-        #print(os.path.basename(input_file))
-        #torchaudio.save(os.path.join(audio_in_path, input_file + '.wav'), full_input_file, 16000)
+        full_output_file = os.path.join(audio_out_path, os.path.basename(input_file) + '-dns4_16k' + '.wav')
 
-    shutil.copyfile(os.path.join(full_input_file), os.path.join(audio_in_path, os.path.basename(input_file) + '-dns4_16k-orig' + '.wav'))
+    shutil.copyfile(os.path.join(full_input_file), os.path.join(audio_in_path, os.path.basename(input_file) + '-dns4_16k' + '.wav'))
+    save_spectro_image(os.path.join(audio_in_path, os.path.basename(input_file) + '-dns4_16k' + '.wav'))
     model = separator.from_hparams(source="speechbrain/sepformer-dns4-16k-enhancement",
                                    savedir='pretrained_models/sepformer-dns4-16k-enhancement')
     est_sources = model.separate_file(full_input_file) # autoconverts to 16kHz
     print(full_output_file)
     torchaudio.save(full_output_file, est_sources[:, :, 0].detach().cpu(), 16000)
-
-    #sf.write(full_output_file, est_sources[:, :, 0][0], 16000, subtype='PCM_16')
-    # print(f"File restored with 'speechbrain/sepformer-dns4-16k-enhancement' and saved as "\
-    #     f"'{input_file}-dns4-16k-res.wav' in '{audio_data_folder}/audio_out' folder.")
+    save_spectro_image(full_output_file)
 
     return wav_to_spectro(full_output_file, 16000), full_output_file
 
@@ -138,3 +110,22 @@ def wav_to_spectro(wav_file_path: str, sr: int):
     spectrogram = pp.waveform_2_spectrogram (audio_arr, sr)
 
     return spectrogram, audio_arr, sr
+
+
+def save_spectro_image(audio_file):
+    x, sr = librosa.load(audio_file) #, sr=46000)
+    mel_signal = librosa.feature.melspectrogram(y=x,  sr=sr, n_mels=256, n_fft=2048)
+    spectrogram = np.abs(mel_signal)
+    power_to_db = librosa.power_to_db(spectrogram, ref=np.max)
+    plt.figure(figsize=(8, 7))
+    librosa.display.specshow(power_to_db,
+                             sr=sr,
+                             x_axis='time',
+                             y_axis='mel',
+                             cmap='magma',
+                             )
+    plt.title('Mel-Spectrogram (dB)', fontdict=dict(size=17))
+    plt.colorbar(label='dB')
+    plt.xlabel('Time', fontdict=dict(size=15))
+    plt.ylabel('Frequency', fontdict=dict(size=15))
+    plt.savefig(audio_file+'.jpg')
